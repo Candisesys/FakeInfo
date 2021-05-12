fe28c4978617473f04113d2b0f8206bfb5aba6e5
Candisesys <johnstove482@gmail.com>
2021-05-11 23:38:06 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let rndNumber = (start, end) => {
+    return Math.floor((Math.random() * (end - start)) + start);
+}
+function replaceAll(str, find, replace) {
+    var escapedFind=find.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+    return str.replace(new RegExp(escapedFind, 'g'), replace);
+}
+
+let fakeName = faker.name.findName();
+let fakeMail = faker.internet.email();
+let fakeYear = rndNumber(1980, 1995);
+let date = new Date();
+/*
+console.log(fakeName);
+console.log(rndNumber(1980, 1995));
+*/
+window.onload = () => {
+    // Elements
+    let info = document.getElementById("info");
+    let copy = document.getElementById("copy");
+    // Scripting
+    info.innerHTML = `Hi! My name is ${fakeName}. I was born in ${fakeYear}, which makes me ${parseInt(date.getFullYear()) - parseInt(fakeYear)} years old. <br/> Email me at ${fakeMail}`;
+    copy.onclick = () => {
+        let copied = info.innerHTML.replace(" <br> ", " ");
+        navigator.clipboard.writeText(copied);
+    }
+}
